936064381aae015e46c242f8b19379208c378d56
alemassim0 <alessandromassimo04@gmail.com>
2026-07-04 19:03:52 +0200

    aggiunta servlet homepage

---
+++ src/main/java/energy_dealer.application.energydealer/Controller/HomePageServlet.java
@@ -0,0 +1,36 @@
+package energy_dealer.application.energydealer.Controller;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import sentimental_sips.application.sentimentalsips.Model.Entity.Carello;
+import sentimental_sips.application.sentimentalsips.Model.Service.ProdottoService;
+import java.io.IOException;
+
+@WebServlet(name = "HomePageServlet", value = "/HomePage")
+public class HomePageServlet extends HttpServlet {
+
+    private ProdottoService prodottoService;
+
+    public void init() {
+        prodottoService = new ProdottoService();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession(false) == null)
+            request.getSession();
+
+        if (request.getSession(false).getAttribute("carrello") == null)
+            request.getSession().setAttribute("carrello", new Carrello());
+
+        prodottoService.getProductsForHomePage(request, response);
+
+        getServletContext().getRequestDispatcher("/View/homePage.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+
+    }
+}
