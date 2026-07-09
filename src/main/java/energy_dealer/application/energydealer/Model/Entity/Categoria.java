04333883840b607e68fea122ac4217e95af76481
alemassim0 <alessandromassimo04@gmail.com>
2026-07-09 17:45:55 +0200

    aggiunta entity categoria

---
+++ src/main/java/energy_dealer/application/energydealer/Model/Entity/Categoria.java
@@ -0,0 +1,28 @@
+package energy_dealer.application.energydealer.Model.Entity;
+
+public class Categoria {
+
+    private int categoria_id;
+    private String nome;
+
+    public Categoria(int categoria_id, String nome) {
+        this.categoria_id = categoria_id;
+        this.nome = nome;
+    }
+
+    public int getCategoria_id() {
+        return categoria_id;
+    }
+
+    public void setCategoria_id(int categoria_id) {
+        this.categoria_id = categoria_id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
