de475b622c2ae4e1156714d153f2d9e99fbccb53
alemassim0 <alessandromassimo04@gmail.com>
2026-07-11 18:10:08 +0200

    aggiunta implementazione ArticoloOrdinatoDAO

---
+++ src/main/java/energy_dealer/application/energydealer/Model/DAOImpl/ArticoloOrdinatoDAOImpl.java
@@ -0,0 +1,34 @@
+package energy_dealer.application.energydealer.Model.DAOImpl;
+
+import energy_dealer.application.energydealer.Model.DAO.ArticoloOrdinatoDAO;
+import energy_dealer.application.energydealer.Model.Entity.ArticoloOrdinato;
+
+import java.util.List;
+
+public class ArticoloOrdinatoDAOImpl implements ArticoloOrdinatoDAO {
+
+    @Override
+    public ArticoloOrdinato findArticoloOrdinatoId(int articolo_ordinato_id) {
+        return null;
+    }
+
+    @Override
+    public List<ArticoloOrdinato> findAllArticoloOrdinato() {
+        return List.of();
+    }
+
+    @Override
+    public void saveArticoloOrdinato(ArticoloOrdinato articolo_ordinato) {
+
+    }
+
+    @Override
+    public void updateArticoloOrdinato(ArticoloOrdinato articolo_ordinato) {
+
+    }
+
+    @Override
+    public void deleteArticoloOrdinato(ArticoloOrdinato articolo_ordinato) {
+
+    }
+}
